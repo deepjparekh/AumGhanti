5104036108e079aa180a9660e97ad25969571943
deepjparekh <deepjparekh@gmail.com>
2024-09-07 12:47:34 -0400

    Added ghanti compose

--- composeApp/src/androidMain/kotlin/org/deepparekh/aumghanti/AumGhanti.kt
+++ composeApp/src/androidMain/kotlin/org/deepparekh/aumghanti/AumGhanti.kt
@@ -1,4 +1,40 @@
 package org.deepparekh.aumghanti
 
-class AumGhanti {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AumGhanti(modifier: Modifier = Modifier) {
+    MaterialTheme {
+        Box(
+            modifier = modifier
+                .fillMaxSize()
+                // todo: make this honor system theme
+                .background(color = Color.White)
+        ) {
+            Image(
+                // todo: remove white background from image asset
+                painter = painterResource(id = R.drawable.ghanti_image),
+                contentDescription = "Aum ghanti image",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        horizontal = 60.dp,
+                        vertical = 144.dp
+                    )
+            )
+        }
+
+    }
 }
