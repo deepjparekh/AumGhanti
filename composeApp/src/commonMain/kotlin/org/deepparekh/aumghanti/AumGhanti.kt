655057add272ee4e8ebd3e9df778927d50a8acc1
Deep <deepjparekh@gmail.com>
2025-07-22 07:31:21 -0400

    Dark mode support and higer res ghanti image

--- composeApp/src/commonMain/kotlin/org/deepparekh/aumghanti/AumGhanti.kt
+++ composeApp/src/commonMain/kotlin/org/deepparekh/aumghanti/AumGhanti.kt
@@ -2,6 +2,7 @@ package org.deepparekh.aumghanti
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -10,28 +11,29 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import aumghanti.composeapp.generated.resources.Res
-import aumghanti.composeapp.generated.resources.ghanti_image
+import aumghanti.composeapp.generated.resources.ghanti
 import org.jetbrains.compose.resources.painterResource
 import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun AumGhanti(modifier: Modifier = Modifier) {
     val viewModel = koinViewModel<GhantiViewModel>()
+    val backgroundColor = if (isSystemInDarkTheme()) {
+        Color.Black
+    } else Color.White
     Box(
         modifier = modifier
             .fillMaxSize()
-            // todo: make this honor system theme
-            .background(color = Color.White)
+            .background(color = backgroundColor)
     ) {
         Image(
-            // todo: remove white background from image asset
-            painter = painterResource(Res.drawable.ghanti_image),
+            painter = painterResource(Res.drawable.ghanti),
             contentDescription = "Aum ghanti image",
             modifier = Modifier
                 .fillMaxSize()
                 .padding(
-                    horizontal = 60.dp,
-                    vertical = 144.dp
+                    horizontal = 32.dp,
+                    vertical = 40.dp
                 )
         )
     }
